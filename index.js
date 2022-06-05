0e3fbdf55aec7c5f610e9770451dc65a0fa99715
Owsama Hajjar <hajjar.owsama@outlook.com>
2022-06-05 18:08:49 +0100

    created Vidly API handlers

--- index.js
+++ index.js
@@ -7,75 +7,72 @@ app.use(express.json());
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
-const courses = [
-    { id: 1, name: "course1" },
-    { id: 2, name: "course2" },
-    { id: 3, name: "course3" },
-];
+const genres = [
+    { id: 1, name: "action" },
+    { id: 2, name: "thriller" },
+    { id: 3, name: "fantasy" },
+    { id: 4, name: "sci-Fi" },
+]
 
-
-// ********* GET Request handlers *********
-app.get('/', (req, res) => {
-    res.send('Hello World')
-});
-
-
-app.get('/api/courses', (req, res) => {
-    res.send(courses)
-});
-
-app.get('/api/courses/:id', (req, res) => {
-    const course = courses.find(c => c.id === parseInt(req.params.id));
-    if (!course) return res.status(404).send('Course id not found');
-    res.send(course)
-});
-
-// ********* POST Request handlers *********
-app.post('/api/courses', (req, res) => {
-
-    const { error } = validateCourse(req.body)
-    if (error) return res.status(400).send(error.details[0].message);
-
-    const course = {
-        id: courses.length + 1,
-        name: req.body.name
-
-    };
-
-    courses.push(course);
-    res.send(course);
-})
-
-// ********* PUT Request handlers *********
-app.put('/api/courses/:id', (req, res) => {
-    const course = courses.find(c => c.id === parseInt(req.params.id));
-    if (!course) return res.status(404).send('Course id not found');
-
-    const { error } = validateCourse(req.body);
-    if (error) return res.status(400).send(error.details[0].message);
-
-    course.name = req.body.name;
-    res.send(course);
-});
-
-// ********* Delete Request handlers *********
-app.delete('/api/courses/:id', (req, res) => {
-    const course = courses.find(c => c.id === parseInt(req.params.id));
-    if (!course) return res.status(404).send('Course id not found');
-
-    const index = courses.indexOf(course);
-    courses.splice(index, 1);
-
-    res.send(course);
-});
-
-
-// ********* Functions *********
-function validateCourse(course) {
+function validateGenre(genre) {
     const schema = Joi.object({
         name: Joi.string().min(3).required()
     });
 
-    return schema.validate(course)
+    return schema.validate(genre)
 }
 
+// ******* GET Request handler *******
+app.get('/api/genres', (req, res) => {
+    res.status(200).send(genres)
+})
+
+app.get('/api/genres/:id', (req, res) => {
+    const genre = genres.find(g => g.id === parseInt(req.params.id));
+    if (!genre) return res.status(404).send('genre id not found')
+    res.status(200).send(genre)
+})
+
+app.get('/api/genre/:name', (req, res) => {
+    const genre = genres.find(g => g.name === req.params.name);
+    if (!genre) return res.status(404).send('genre name not found')
+    res.status(200).send(genre)
+})
+
+// ******* Post Request handler *******
+app.post('/api/genres', (req, res) => {
+    const { error } = validateGenre(req.body)
+    if (error) return res.status(400).send(error.details[0].message);
+
+    const genre = {
+        id: genres.length + 1,
+        name: req.body.name
+    }
+
+    genres.push(genre)
+    res.send(genre)
+})
+
+// ******* Update Request handler *******
+
+app.put('/api/genres/:id', (req, res) => {
+    const { error } = validateGenre(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    const genre = genres.find(g => g.id === parseInt(req.params.id));
+
+    genre.name = req.body.name;
+    res.send(genre)
+})
+
+// ******* Delete Request handler *******
+app.delete('/api/genres/:id', (req, res) => {
+    const genre = genres.find(g => g.id === parseInt(req.params.id))
+    if (!genre) return res.status(404).send('genre name not found')
+
+    const index = genres.indexOf(genre);
+    genres.splice(index, 1)
+
+    res.send(genre)
+
+})
